07dd1451dea6cb309f4f1dbbb542b35f103e1845
igormask <129780415+igormask@users.noreply.github.com>
2023-11-27 19:08:30 +0200

    git-commit

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const refs = {
     openModalBtn: document.querySelector('[data-action="open-modal"]'),
-    // closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
     backdrop: document.querySelector('.js-backdrop'),
 };
 
